ec036328d077b1de123fdbb062aee6c43dde5aa2
Shroff <shroff.kandarp@gmail.com>
2018-03-15 09:04:25 +0530

    Initial SetUp

--- app/src/main/java/com/dreamplay/FantasyPointsActivity.java
+++ app/src/main/java/com/dreamplay/FantasyPointsActivity.java
@@ -11,5 +11,8 @@ public class FantasyPointsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fantasy_points);
 
 //        getFantasyPoints//iMemberId
+//        http://ec2-18-220-193-12.us-east-2.compute.amazonaws.com/phpMyAdmin/sql.php?db=dream_play&table=points_type&pos=0
+
+//        https://drive.google.com/file/d/1-5Ha2zB_0SIV5W7IsHOZb8V-1ITIr8Cs/view?usp=sharing
     }
 }
